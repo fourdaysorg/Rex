91bd019a3a8a7c14d329f645e19d0f305c61e9a8
RichoM <richi.moran@gmail.com>
2019-07-26 17:22:55 -0300

    Added a static list of campaigns to populate the list.

--- src/RexWeb/Controllers/CatalogController.cs
+++ src/RexWeb/Controllers/CatalogController.cs
@@ -1,16 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using System.Collections.Generic;
+using RexWeb.Models;
+using System.Linq;
 
-namespace MvcMovie.Controllers
+namespace RexWeb.Controllers
 {
     public class CatalogController : Controller
     {
-        // 
-        // GET: /Catalog/
+        static IEnumerable<CampaignModel> Campaigns 
+        {
+            get
+            {
+                return Enumerable.Range(0, 250)
+                    .Select(ign => new CampaignModel() 
+                    {
+                        Name = "Huracán Katrina",
+                        Description = "El huracán Katrina fue uno de los más destructivos y el que causó más víctimas mortales de la temporada de huracanes en el Atlántico de 2005."
+                    });
+            }
+        }
 
         public IActionResult Index()
         {
-            return View();
+            return View(Campaigns);
         }
     }
 }
