f576f69294fc2eee75e833ffc604b21179904d9b
Matias Teragni <matias.teragni@gmail.com>
2019-07-27 15:49:49 -0300

    Added buttons to increase and decrease the donated amount

--- mobile/rexApp/src/app/donate/donate.page.ts
+++ mobile/rexApp/src/app/donate/donate.page.ts
@@ -15,7 +15,7 @@ export class DonatePage implements OnInit {
       if (this.router.getCurrentNavigation().extras.state) {
         this.campaign = this.router.getCurrentNavigation().extras.state.campaign;
         this.donations = this.campaign.AvailableItems.map((i) => {
-          return { persistent:true, name: i.name+":", icon: i.icon, count: 0 };
+          return { persistent: true, name: i.name + ":", icon: i.icon, count: 0 };
         });
       }
     });
@@ -24,5 +24,13 @@ export class DonatePage implements OnInit {
 
   ngOnInit() {
   }
+  increase(item) {
+    item.count++;
+  }
 
+  decrease(item) {
+    if (item.count > 0) {
+      item.count--;
+    }
+  }
 }
