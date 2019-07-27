edcf6fd664076bb2c87f1f5c72d0cdc75c80027e
Matias Teragni <matias.teragni@gmail.com>
2019-07-27 12:47:43 -0300

    Unhardocded the existing campaign.

---
+++ mobile/rexApp/src/app/campaign.ts
@@ -0,0 +1,14 @@
+export class Campaign{
+    Name: string;
+    Country: string;
+    Description: string;
+/**
+ *
+ */
+constructor(Name:string, Country:string, Description:string) {
+    this.Name=Name;
+    this.Country=Country;
+    this.Description=Description;
+}
+
+}
--- mobile/rexApp/src/app/campaigns/campaigns.page.ts
+++ mobile/rexApp/src/app/campaigns/campaigns.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Campaign } from '../campaign';
 
 @Component({
   selector: 'app-campaigns',
@@ -8,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class CampaignsPage implements OnInit {
 
   constructor() { }
-
+  public campaigns:Campaign[];
   ngOnInit() {
+    this.campaigns = new Array<Campaign>();
+    this.campaigns.push(
+      new Campaign("Hurricane Katrina"
+      ,"U.S.A.",
+      "Extremely destructive and deadly Category 5 hurricane that made landfall on Florida and Louisiana"));
+      
   }
 
 }
