9df7d271971f4e4bc0a3ca9c8b31193c6625bb30
Matias Teragni <matias.teragni@gmail.com>
2019-07-28 09:52:56 -0300

    Added the missing buttons into the confirm-donation page

--- mobile/rexApp/src/app/confirm-donation/confirm-donation.page.ts
+++ mobile/rexApp/src/app/confirm-donation/confirm-donation.page.ts
@@ -9,16 +9,22 @@ import { NavController } from '@ionic/angular';
 })
 export class ConfirmDonationPage implements OnInit {
   donations: any[];
-
-  constructor(private route: ActivatedRoute, private router: Router,private navController: NavController) {
+  waiting = false;
+  finished = false;
+  constructor(private route: ActivatedRoute, private router: Router, private navController: NavController) {
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
-        this.donations =this.router.getCurrentNavigation().extras.state.donations;
+        this.donations = this.router.getCurrentNavigation().extras.state.donations;
       }
     });
-   }
+  }
 
   ngOnInit() {
   }
+  donate() {
+    let me=this;
+    me.waiting=true;
+    setTimeout(()=>{me.waiting=false; me.finished=true;},2000);
 
+  }
 }
