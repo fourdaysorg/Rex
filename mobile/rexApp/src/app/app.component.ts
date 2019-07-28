dbe75e9b2288a8e3edcb5e5495541efac18d9f31
Matias Teragni <matias.teragni@gmail.com>
2019-07-28 11:14:18 -0300

    now the items on the menu depend on the global user state

--- mobile/rexApp/src/app/app.component.ts
+++ mobile/rexApp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { GlobalService } from './global.service';
 
 @Component({
   selector: 'app-root',
@@ -10,44 +11,53 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'Campaigns',
-      url: '/campaigns',
-      icon: 'list'
-    },
-    {
-      title:'Submit Campaign',
-      url:'/lala',
-      icon:'globe'
-    },
-    {
-      title:'My Donations',
-      url:'/lala',
-      icon:'cube'
-    },
-    {
-      title:'Receive Donations',
-      url:'/receive-donation',
-      icon:'heart'
-    },
-    {
-      title:'Settings',
-      url:'/lala',
-      icon:'settings'
-    }
-  ];
+  public appPages = [];
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public global: GlobalService
   ) {
+
+    this.appPages.push({
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    });
+    this.appPages.push({
+      title: 'Campaigns',
+      url: '/campaigns',
+      icon: 'list'
+    });
+    this.appPages.push({
+      title: 'My Donations',
+      url: '/lala',
+      icon: 'cube'
+    });
+    if (global.user.isOng) {
+      this.appPages.push({
+        title: 'Submit Campaign',
+        url: '/lala',
+        icon: 'globe'
+      });
+      this.appPages.push({
+        title: 'Receive Donations',
+        url: '/receive-donation',
+        icon: 'heart'
+      });
+    }
+    this.appPages.push({
+      title: 'Settings',
+      url: '/lala',
+      icon: 'settings'
+    });
+
+
+
+
+
+
     this.initializeApp();
   }
 
