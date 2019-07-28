cba105af7deba9d0ffcb0c1ecbbcc6c1afe1a48e
Matias Teragni <matias.teragni@gmail.com>
2019-07-28 11:54:30 -0300

    Now the homepage has a list of "recent donations"

--- mobile/rexApp/src/app/home/home.page.ts
+++ mobile/rexApp/src/app/home/home.page.ts
@@ -7,9 +7,18 @@ import { GlobalService } from '../global.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public recentDonations: any[]=[];
 
-  constructor(public global:GlobalService) {
-    
+  private names = ["Finn", "Esteban", "Victoria", "Hector", "Joseph", "John", "Marie", "Won", "Christian"];
+  private avatars=["/assets/img/users/avatar1.svg","/assets/img/users/avatar2.svg","/assets/img/users/avatar3.svg","/assets/img/users/avatar4.svg"];
+  constructor(public global: GlobalService) {
+    this.recentDonations.push({
+      name: this.names[0],
+      avatar:this.avatars[0],
+      icon: global.items[0].icon,
+      description: global.items[0].name,
+      amount: 30
+    });
   }
 
 }
