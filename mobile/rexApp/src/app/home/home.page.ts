243484a19bb16f022490e6b85f2227b7b1a7fed7
Matias Teragni <matias.teragni@gmail.com>
2019-07-28 12:00:23 -0300

    Added random generation of the recent donations list

--- mobile/rexApp/src/app/home/home.page.ts
+++ mobile/rexApp/src/app/home/home.page.ts
@@ -7,18 +7,31 @@ import { GlobalService } from '../global.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public recentDonations: any[]=[];
+  public recentDonations: any[] = [];
 
   private names = ["Finn", "Esteban", "Victoria", "Hector", "Joseph", "John", "Marie", "Won", "Christian"];
-  private avatars=["/assets/img/users/avatar1.svg","/assets/img/users/avatar2.svg","/assets/img/users/avatar3.svg","/assets/img/users/avatar4.svg"];
+  private avatars = ["/assets/img/users/avatar1.svg", "/assets/img/users/avatar2.svg", "/assets/img/users/avatar3.svg", "/assets/img/users/avatar4.svg"];
   constructor(public global: GlobalService) {
+    this.createRandomDonation();
+    this.createRandomDonation();
+    this.createRandomDonation();
+  }
+  private createRandomDonation() {
+    let item = this.getRandomItemFrom(this.global.items);
     this.recentDonations.push({
-      name: this.names[0],
-      avatar:this.avatars[0],
-      icon: global.items[0].icon,
-      description: global.items[0].name,
-      amount: 30
+      name: this.getRandomItemFrom(this.names),
+      avatar: this.getRandomItemFrom(this.avatars),
+      icon: item.icon,
+      description: item.name,
+      amount: Math.floor(Math.random() * 40)
     });
+    while(this.recentDonations.length>5)
+    {
+      this.recentDonations.pop();
+    }
+  }
+  private getRandomItemFrom(anArray: any[]): any {
+    return anArray[Math.floor(Math.random() * anArray.length)];
   }
 
 }
